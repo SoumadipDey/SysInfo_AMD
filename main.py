708935bddcd16f31717d3e747b4b61ad7367c4a3
Soumadip Dey <soumadipdey63@gmail.com>
2021-04-11 23:36:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -51,44 +51,27 @@ class MainWindow(QMainWindow):
         ## SHOW ==> MAIN WINDOW
         ########################################################################
         self.show()
-
-        ## CALL THE setAllValues() FUNCTION REPEATEDLY
+        ##initialize Static values
+        self.setStaticValues()
+        ## CALL THE setDynamicValues() FUNCTION REPEATEDLY
 
         self.timer = QTimer(self)
-        self.timer.timeout.connect(self.setAllValues)
-        self.timer.start(100)
+        self.timer.timeout.connect(self.setDynamicValues)
+        self.timer.start(0)
 
     ## SET ALL VALUES
-    def setAllValues(self):
+    def setStaticValues(self):
         all_cpu_data = self.fetch_all_cpu_info()
         all_gpu_data = self.fetch_all_gpu_info()
         all_ram_data = self.fetch_all_ram_info()
 
-        ##SETTING percentage label VALUES
-        htmlTextFormatPercentLabel = """<b>{PERCENT_VALUE}%</b>"""
-        self.ui.usage_label_cpu.setText(htmlTextFormatPercentLabel.replace("{PERCENT_VALUE}",str(all_cpu_data["cpu_usage_percentage"])))
-        self.ui.usage_label_gpu.setText(htmlTextFormatPercentLabel.replace("{PERCENT_VALUE}",str(all_gpu_data["gpu_usage_percentage"])))
-        self.ui.usage_label_ram.setText(htmlTextFormatPercentLabel.replace("{PERCENT_VALUE}",str(all_ram_data["ram_usage_percentage"])))
-
-        ##SETTING Frequency label VALUES of BIG LABELS
-        htmlTextFormatFreqLabelBig = """<p><span style=" font-weight:600;">Freq: </span>{FREQ_VALUE} MHz</p>"""
-        self.ui.freq_label_cpu.setText(htmlTextFormatFreqLabelBig.replace("{FREQ_VALUE}",str(all_cpu_data["cpu_current_freq"])))
-        self.ui.freq_label_gpu.setText(htmlTextFormatFreqLabelBig.replace("{FREQ_VALUE}",str(all_gpu_data["gpu_current_freq"])))
-
-        ##SETTING TEMPERATURE label values
-        htmlTextFormatTemperature = """<p><span style=" font-weight:600;">Temperature:</span> {TEMPERATURE_VALUE} °C</p>"""
-        self.ui.temp_label_cpu.setText(htmlTextFormatTemperature.replace("{TEMPERATURE_VALUE}",str(all_cpu_data["cpu_temp"])))
-        self.ui.temp_label_gpu.setText(htmlTextFormatTemperature.replace("{TEMPERATURE_VALUE}",str(all_gpu_data["gpu_temp"])))
+        ##SETTING Name labels
+        self.ui.label_cpu_name.setText(str(all_cpu_data["cpu_name"]))
+        self.ui.label_gpu_name.setText(str(all_gpu_data["gpu_name"]))
 
         ##SETTING RAM memory label values
         htmlTextFormatRamTotal = """<p><span style=" font-weight:600;">Total:</span> {TOTAL_RAM_VALUE} GB</p>"""
         self.ui.totalSize_label_ram.setText(htmlTextFormatRamTotal.replace("{TOTAL_RAM_VALUE}",str(all_ram_data["ram_total"])))
-        htmlTextFormatRamUsed = """<p><span style=" font-weight:600;">Used:</span> {USED_RAM_VALUE} GB</p>"""
-        self.ui.usedSize_label_ram.setText(htmlTextFormatRamUsed.replace("{USED_RAM_VALUE}",str(all_ram_data["ram_used"])))
-
-        ##SETTING Name labels
-        self.ui.label_cpu_name.setText(str(all_cpu_data["cpu_name"]))
-        self.ui.label_gpu_name.setText(str(all_gpu_data["gpu_name"]))
 
         ##SETTING Max frequency labels
         htmlTextFormatCpuMaxFreq = """<p>Max Frequency: {CPU_MAXFREQ_VALUE} MHz</p>"""
@@ -101,32 +84,72 @@ class MainWindow(QMainWindow):
         self.ui.label_physical_cores.setText(htmlTextFormatCpuPhyCores.replace("{PHYSICAL_CORES_VALUE}",str(all_cpu_data["cpu_cores_physical"])))
         htmlTextFormatCpuLogCores = """<p>Logical Processors: {LOGICAL_CORES_VALUE}</p>"""
         self.ui.label_logical_cores.setText(htmlTextFormatCpuLogCores.replace("{LOGICAL_CORES_VALUE}",str(all_cpu_data["cpu_cores_logical"])))
-            
+
         ##SETTING GPU memory and memory freq labels
         htmlTextFormatGpuMemSize = """<p>Total VRAM: {VRAM_SIZE_VALUE} GB</p>"""
         self.ui.label_gpu_max_vram.setText(htmlTextFormatGpuMemSize.replace("{VRAM_SIZE_VALUE}",str(all_gpu_data["gpu_vram_total"])))
+
+
+
+    def setDynamicValues(self):
+        all_cpu_data = self.fetch_all_cpu_info()
+        all_gpu_data = self.fetch_all_gpu_info()
+        all_ram_data = self.fetch_all_ram_info()
+        app.processEvents()
+
+        ##SETTING percentage label VALUES
+        htmlTextFormatPercentLabel = """<b>{PERCENT_VALUE}%</b>"""
+        self.ui.usage_label_cpu.setText(htmlTextFormatPercentLabel.replace("{PERCENT_VALUE}",str(all_cpu_data["cpu_usage_percentage"])))
+        self.ui.usage_label_gpu.setText(htmlTextFormatPercentLabel.replace("{PERCENT_VALUE}",str(all_gpu_data["gpu_usage_percentage"])))
+        self.ui.usage_label_ram.setText(htmlTextFormatPercentLabel.replace("{PERCENT_VALUE}",str(all_ram_data["ram_usage_percentage"])))
+        app.processEvents()
+
+        ##SETTING Frequency label VALUES of BIG LABELS
+        htmlTextFormatFreqLabelBig = """<p><span style=" font-weight:600;">Freq: </span>{FREQ_VALUE} MHz</p>"""
+        self.ui.freq_label_cpu.setText(htmlTextFormatFreqLabelBig.replace("{FREQ_VALUE}",str(all_cpu_data["cpu_current_freq"])))
+        self.ui.freq_label_gpu.setText(htmlTextFormatFreqLabelBig.replace("{FREQ_VALUE}",str(all_gpu_data["gpu_current_freq"])))
+        app.processEvents()
+
+        ##SETTING TEMPERATURE label values
+        htmlTextFormatTemperature = """<p><span style=" font-weight:600;">Temperature:</span> {TEMPERATURE_VALUE} °C</p>"""
+        self.ui.temp_label_cpu.setText(htmlTextFormatTemperature.replace("{TEMPERATURE_VALUE}",str(all_cpu_data["cpu_temp"])))
+        self.ui.temp_label_gpu.setText(htmlTextFormatTemperature.replace("{TEMPERATURE_VALUE}",str(all_gpu_data["gpu_temp"])))
+        app.processEvents()
+
+        ##SETTING RAM memory label values
+        htmlTextFormatRamUsed = """<p><span style=" font-weight:600;">Used:</span> {USED_RAM_VALUE} GB</p>"""
+        self.ui.usedSize_label_ram.setText(htmlTextFormatRamUsed.replace("{USED_RAM_VALUE}",str(all_ram_data["ram_used"])))
+        app.processEvents()
+
+        ##SETTING GPU memory and memory freq labels
         htmlTextFormatGpuMemFreq = """<p>Memory Freq: {GPU_MEMFREQ_VALUE} MHz</p>"""
         self.ui.label_gpy_mem_freq.setText(htmlTextFormatGpuMemFreq.replace("{GPU_MEMFREQ_VALUE}",str(all_gpu_data["gpu_memory_freq"])))
+        app.processEvents()
 
         self.progressBarValue(all_cpu_data["cpu_usage_percentage"],self.ui.circularProgress_CPU,"rgba(085, 170, 255, 255)")
         self.progressBarValue(all_gpu_data["gpu_usage_percentage"],self.ui.circularProgress_GPU,"rgba(255, 040, 090, 255)")
         self.progressBarValue(all_ram_data["ram_usage_percentage"],self.ui.circularProgress_RAM,"rgba(170, 000, 255, 255)")
+        app.processEvents()
 
     def fetch_all_cpu_info(self):
         ## CPU INFO FETCH ##
         ####################
         #Fetch exact name of cpu
         cpu_name = str(cpuinfo.get_cpu_info()["brand_raw"].replace("with","").replace("Radeon","").replace("Vega","").replace("Graphics","").strip())
+        app.processEvents()
 
         #Physical cores
         cpu_cores_physical = int(psutil.cpu_count(logical=False))
+        app.processEvents()
 
         #Logical cores
         cpu_cores_logical = int(psutil.cpu_count(logical=True))
+        app.processEvents()
 
         # CPU frequency Max
         cpufreq = psutil.cpu_freq()
         cpu_max_freq = int(cpufreq.max)
+        app.processEvents()
 
         #current frequency
         w = wmi.WMI(namespace="root\OpenHardwareMonitor")
@@ -141,9 +164,11 @@ class MainWindow(QMainWindow):
         if(count != 0):
             cpu_current_freq = cpu_current_freq / count
         cpu_current_freq = int(m.ceil(cpu_current_freq))
+        app.processEvents()
 
         #CPU usage overall
         cpu_usage_percentage = int(m.floor(psutil.cpu_percent()))
+        app.processEvents()
 
         #CPU Temperature
         cpu_temp = 0
@@ -155,6 +180,7 @@ class MainWindow(QMainWindow):
                 if(sensor.Name == "CPU Package"):
                     cpu_temp = sensor.Value
         cpu_temp = int(m.ceil(cpu_temp))
+        app.processEvents()
 
         all_cpu_data = {
             "cpu_name" : cpu_name,
@@ -165,6 +191,7 @@ class MainWindow(QMainWindow):
             "cpu_usage_percentage" : cpu_usage_percentage,
             "cpu_temp" : cpu_temp
         }
+        app.processEvents()
 
         return all_cpu_data
 
@@ -177,11 +204,13 @@ class MainWindow(QMainWindow):
             gpu = str(device.adapterName)
             gpu = gpu.replace('b','')
             gpu_name = gpu.replace("'","").replace("Series","")
-            
+        app.processEvents()
+
         #Fetch GPU VRAM size
         for adapter in pyamd_adl.adapters:
             memory = adapter.memory
             gpu_vram_total = int((memory.size)/(1024**3))
+        app.processEvents()
 
         #Fetch GPU core clocks along with maximum and minimum clocks
         for device in devices:
@@ -197,6 +226,7 @@ class MainWindow(QMainWindow):
 
         #fetch GPU memory freq
             gpu_memory_freq = int(device.getCurrentMemoryClock())
+        app.processEvents()
 
         all_gpu_data = {
             "gpu_name": gpu_name,
@@ -216,6 +246,7 @@ class MainWindow(QMainWindow):
         available_system_memory = round(psutil.virtual_memory()[1]/(1024**3),1)
         used_system_memory = round((total_system_memory - available_system_memory),1)
         percentage_used_system_memory = int(psutil.virtual_memory()[2])
+        app.processEvents()
 
         all_ram_data = {
             "ram_total" : total_system_memory,
@@ -223,7 +254,8 @@ class MainWindow(QMainWindow):
             "ram_used" : used_system_memory,
             "ram_usage_percentage" : percentage_used_system_memory
         }
-
+        app.processEvents()
+        
         return all_ram_data
 
     def progressBarValue(self, value, frame_widget, color):
